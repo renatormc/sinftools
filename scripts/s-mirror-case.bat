f65d49d63f44f416af146a04d12ed662781b9704
renatormc <renatomartinsrmc@gmail.com>
2020-07-17 14:20:39 -0300

    Normal

--- scripts/s-mirror-case.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-s-mark sinf-mirror -c %1 -f "%TEMP%\sinf_mirror_input.txt" && ^
-s-mirror -f "%TEMP%\sinf_mirror_input.txt"
--- tools/liga/cmdtool/scripts/synkdir.bat
+++ tools/liga/cmdtool/scripts/synkdir.bat
@@ -1,3 +1,3 @@
 @echo off
-REM s-mirror-case nome_do_caso
+REM s-mirror -c nome_do_caso
 s-mirror -s "C:\path\source\dir" -d "C:\path\dest\dir"
