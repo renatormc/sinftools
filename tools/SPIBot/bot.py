b44cdfa13677c39b53a4754a250fe3cc677ac835
renatormc <renatomartinsrmc@gmail.com>
2020-07-24 16:09:11 -0300

    -m

---
+++ tools/SPIBot/bot.py
@@ -0,0 +1,99 @@
+import re
+import sys
+import os
+import string
+
+try:
+    sys.path.append(os.path.join(os.environ['ANDROID_VIEW_CLIENT_HOME'], 'src'))
+except:
+    pass
+
+from com.dtmilano.android.viewclient import ViewClient
+
+
+def dropDownMenu(device,numberOfTimes):
+    for i in range(0,numberOfTimes):
+        device.press('KEYCODE_DPAD_DOWN')
+
+class Bot:
+    def __init__(self):
+        self.MENU_MAIS = "Mais"
+        self.EXPORTAR = 'Exportar conversa'
+        self.INCLUIR_MIDIA = "INCLUIR ARQUIVOS DE MÍDIA"
+        self.APP_NAME = "Extrator 0.4"
+        self.SALVAR = "SALVAR"
+
+
+    def extract(self):
+        device, serialno = ViewClient.connectToDeviceOrExit()
+
+        ViewClient.sleep(2)
+
+        vc = ViewClient(device, serialno)
+
+
+
+        listContatos = []
+        repeticoes = 0
+
+        while repeticoes < 10:
+            vc.dump(window=-1)
+            textViews = vc.findViewsWithAttribute("class", "android.widget.TextView")
+            listaAtualizada = False
+            for texView in textViews:
+                if(texView.getId() == 'com.whatsapp:id/conversations_row_contact_name'):
+                    contato = texView.getText()
+                    if contato not in listContatos:
+                        listContatos.append(texView.getText())
+                        print(f"Exportando dados do contato: {contato}")
+                        listaAtualizada = True
+                        try:
+                            vc.sleep(1)
+                            texView.touch()
+                            vc.sleep(1)
+                            vc.dump(window=-1)
+                            vc.findViewWithContentDescriptionOrRaise(u'''Mais opções''').touch()
+                            vc.dump(window=-1)
+                            vc.findViewWithTextOrRaise(self.MENU_MAIS).touch()
+                            vc.dump(window=-1)
+                            try:
+                                vc.findViewWithTextOrRaise(self.EXPORTAR).touch()
+                                vc.sleep(1)
+                                vc.dump(window=-1)
+                                try:
+                                    vc.findViewWithTextOrRaise(self.INCLUIR_MIDIA).touch()
+                                    vc.sleep(3)
+                                except:
+                                    print("Contato sem mídias ")
+                                try:
+                                    vc.dump(window=-1)
+                                    vc.findViewWithTextOrRaise(self.APP_NAME).touch()
+                                except:
+                                    # A exportação pode ser demorada
+                                    vc.sleep(15)
+                                    vc.dump(window=-1)
+                                    vc.findViewWithTextOrRaise(self.APP_NAME).touch()
+                                    print("Exportação demorando mais que o normal ")
+                                vc.sleep(1)
+                                vc.dump(window=-1)
+                                vc.findViewWithTextOrRaise(self.SALVAR).touch()
+                                vc.sleep(2)
+                                device.press('KEYCODE_ESCAPE')
+                                vc.sleep(1)
+                                device.press('KEYCODE_ESCAPE')
+                            except Exception as e:
+                                print("Opção de enviar por e-mail não encontrada! ")
+                                device.press('KEYCODE_ESCAPE')
+                                vc.sleep(1)
+                                device.press('KEYCODE_ESCAPE')
+                                print (e)
+                        except Exception as e:
+                            print(f"View não encontrada: {contato}")
+                            print (e)
+            if not listaAtualizada:
+                repeticoes = repeticoes + 1
+            dropDownMenu(device,5)
+
+        for contato in listContatos:
+            print (contato.encode('utf-8'))
+            
--- tools/SPIBot/cmd.py
+++ tools/SPIBot/cmd.py
@@ -2,6 +2,8 @@ import click
 import subprocess
 import config
 import shutil
+import os
+
 
 @click.group()
 @click.pass_context
@@ -12,10 +14,12 @@ def cli(ctx):
 @cli.command()
 @click.option("--app", type=click.Choice(['spi', 'extractor']), default='extractor')
 def install(app):
-    apk = config.app_dir / "EXTRATOR_0.4.apk" if app == 'extractor' else config.app_dir / 'spitools-1.2.apk'
+    apk = config.app_dir / \
+        "EXTRATOR_0.4.apk" if app == 'extractor' else config.app_dir / 'spitools-1.2.apk'
     print(f"Instalando {apk.name}")
     args = ['adb', 'install', str(apk)]
     subprocess.run(args)
+
 
 @cli.command()
 def extract():
@@ -23,6 +27,11 @@ def extract():
     shutil.copy(script, "SPIWhatsAppExtracaoManual.py")
     args = ['s-idlex', "SPIWhatsAppExtracaoManual.py"]
     subprocess.run(args)
+
+@cli.command()
+def apk():
+    shutil.copy(config.app_dir / "EXTRATOR_0.4.apk", "EXTRATOR_0.4.apk")
+    shutil.copy(config.app_dir / "EXTRATOR_0.4.apk", 'spitools-1.2.apk')
 
 
 if __name__ == '__main__':
