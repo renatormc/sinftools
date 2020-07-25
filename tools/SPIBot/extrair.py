ff4bb10d48ef0611445c5bf7fd38346d3eabccaf
renatormc <renatomartinsrmc@gmail.com>
2020-07-25 09:11:24 -0300

    Normal

--- tools/SPIBot/extrair.py
+++ tools/SPIBot/extrair.py
@@ -8,6 +8,7 @@ bot.INCLUIR_MIDIA = "INCLUIR ARQUIVOS DE MÍDIA"
 bot.APP_NAME = "Extrator 0.4"
 bot.SALVAR = "SALVAR"
 bot.VOLTAR = "KEYCODE_BACK"
+# bot.VOLTAR = "KEYCODE_ESCAPE"
 bot.ROLAR_N_VEZES = 5
 
 bot.extract()
