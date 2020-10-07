c6960bf9b4838c619050bf47699af52c87141612
renatormc <renatomartinsrmc@gmail.com>
2020-10-07 15:33:39 -0300

    Normal

--- tools/sinfpics/config.py
+++ tools/sinfpics/config.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 app_dir = Path(os.path.dirname(os.path.realpath(__file__)))
 sinftools_dir = Path(os.getenv("SINFTOOLS"))
-debug = True
+debug = False
 url_sinfweb = "http://localhost:8000" if debug else "http://10.129.3.14"
