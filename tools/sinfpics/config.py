1c2944cfa4edacd5a3a18c833d8e92c6675bf3dc
renatormc <renatomartinsrmc@gmail.com>
2020-10-07 16:52:45 -0300

    Normal

--- tools/sinfpics/config.py
+++ tools/sinfpics/config.py
@@ -1,7 +1,11 @@
 import os
 from pathlib import Path
+import sys
 
 app_dir = Path(os.path.dirname(os.path.realpath(__file__)))
 sinftools_dir = Path(os.getenv("SINFTOOLS"))
 debug = False
+if len(sys.argv) > 1 and sys.argv[1] == 'debug':
+    print("Modo debug ligado")
+    debug = True
 url_sinfweb = "http://localhost:8000" if debug else "http://10.129.3.14"
