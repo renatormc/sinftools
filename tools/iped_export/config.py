e24299c534e686a9af8484931910ea97d21d8d53
renatormc <renatomartinsrmc@gmail.com>
2020-11-12 14:10:43 -0300

    Correção ipedexport

--- tools/iped_export/config.py
+++ tools/iped_export/config.py
@@ -4,4 +4,4 @@ import os
 
 app_dir = Path(os.path.dirname(os.path.realpath(__file__)))
 sinftools_dir = Path(os.getenv("SINFTOOLS"))
-jarfile = app_dir / "ipedexport-1.0-SNAPSHOT.jar"
+jarfile = app_dir / "ipedexport.jar"
